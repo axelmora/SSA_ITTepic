79200e5b5b77f8f6b58def8449f1eedb06968e5d
fernando <feranimaciones@gmail.com>
2017-10-21 23:53:10 -0600

    Act

--- application/views/ssainicio.php
+++ application/views/ssainicio.php
@@ -129,7 +129,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 				</a>
 			</div>
 		</div>
-		
 	<?php $this->load->view('include/footer'); ?>
 </body>
 <script type="text/javascript" src="<?php echo base_url(); ?>js/jquery-3.2.1.min.js"></script>
