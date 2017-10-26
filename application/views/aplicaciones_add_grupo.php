c1446812e8fb2b70b904aa46d6230c756b0510b6
fernando <feranimaciones@gmail.com>
2017-10-25 20:01:46 -0600

    Seleccion Alumnos

--- application/views/aplicaciones_add_grupo.php
+++ application/views/aplicaciones_add_grupo.php
@@ -19,6 +19,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <link href="<?php echo base_url(); ?>css/fontello.css" type="text/css" rel="stylesheet" />
   <link href="<?php echo base_url(); ?>css/dataTables.bootstrap4.min.css" type="text/css" rel="stylesheet" />
   <link href="<?php echo base_url(); ?>css/responsive.bootstrap4.min.css" type="text/css" rel="stylesheet" />
+  <link href="<?php echo base_url(); ?>css/dataTables.checkboxes.css" type="text/css" rel="stylesheet" />
 </head>
 <body>
   <?php $this->load->view('include/menu'); ?>
@@ -29,7 +30,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
           <div class="card-body">
             <div class="row">
               <div class="col-lg-12">
-                <h3><i class="icon-clipboard" aria-hidden="true"></i>Crear nuevo grupo de aplicacion <?php// echo "$peridotlista";  ?> </h3>
+                <h3><i class="icon-clipboard" aria-hidden="true"></i>Crear nuevo grupo de aplicacion <!-- <?php// echo "$peridotlista";  ?> --> </h3>
               </div>
             </div>
             <br>
@@ -139,7 +140,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             <table id="tablaSeleccionAlumnos" class="table table-striped table-bordered dt-responsive tablaletradocentes " cellspacing="0" width="100%">
               <thead>
                 <tr>
-                  <th><input type="checkbox" name="select_all" value="1" id="example-select-all"></th>
+                  <th><!--<input type="checkbox" name="select_all" value="1" id="example-select-all">--></th>
                   <th>NUMERO CONTROL</th>
                   <th>NOMBRE</th>
                   <th>CODIGO</th>
@@ -152,19 +153,75 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                   ?>
                   <tr>
                     <td><?php echo "".utf8_decode($valores->numero_control);?></td>
-                  <td>
-                    <?php echo "".utf8_decode($valores->numero_control); ?>
-                  </td>
-                  <td>
-                    <?php echo "".($valores->nombre); ?>
-                  </td>
-                  <td>
-                    <?php echo "".utf8_decode($valores->codigo); ?>
-                  </td>
-                  <td>
-                    <?php echo "".($valores->carrera); ?>
-                  </td>
+                    <td>
+                      <?php echo "".utf8_decode($valores->numero_control); ?>
+                    </td>
+                    <td>
+                      <?php echo "".($valores->nombre); ?>
+                    </td>
+                    <td>
+                      <?php echo "".utf8_decode($valores->codigo); ?>
+                    </td>
+                    <td>
+                      <?php echo "".($valores->carrera); ?>
+                    </td>
 
+                  </tr>
+                  <?php
+                }
+                ?>
+              </tbody>
+            </table>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-danger" data-dismiss="modal">CANCELAR</button>
+            <button  type="submit" class="btn btn-success">ACPETAR SELECCIONADOS </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </form>
+  <!-- MODAL ALUMNOS FIN  -->
+  <pre id="example-console">
+  </pre>
+  <!-- MODAL DOCENTES  -->
+  <div class="modal fade" id="modalDocentes" tabindex="-1" role="dialog" aria-labelledby="modalDocentes" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Seleccionar docente.</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <table id="tablaSeleccionDocentes" class="table table-striped table-bordered dt-responsive tablaletradocentes " cellspacing="0" width="100%">
+            <thead>
+              <tr>
+                <th>MATERIA</th>
+                <th>DEPARTAMENTO</th>
+                <th>OPCIONes</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php
+              foreach ($Docentes as $key => $valores) {
+                ?>
+                <tr>
+                  <td>
+                    <?php echo "".utf8_decode($valores->nombres)." ".utf8_decode($valores->apellidos); ?>
+                  </td>
+                  <td>
+                    <?php echo "".utf8_decode($valores->departamento); ?>
+                  </td>
+                  <td>
+                    <center>
+                      <button type="button" onclick="selecionarDoc('<?php echo $valores->rfc; ?>','<?php echo "".utf8_decode($valores->nombres)." ".utf8_decode($valores->apellidos)?>')" class="btn btn-info" >
+                        <i class="fa  fa-check-square" aria-hidden="true"></i>
+
+                      </button>
+                    </center>
+                  </td>
                 </tr>
                 <?php
               }
@@ -174,147 +231,91 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-danger" data-dismiss="modal">CANCELAR</button>
-          <button  type="submit" class="btn btn-success">ACPETAR SELECCIONADOS </button>
         </div>
       </div>
     </div>
   </div>
-</form>
-<!-- MODAL ALUMNOS FIN  -->
-<pre id="example-console">
-</pre>
-<!-- MODAL DOCENTES  -->
-<div class="modal fade" id="modalDocentes" tabindex="-1" role="dialog" aria-labelledby="modalDocentes" aria-hidden="true">
-  <div class="modal-dialog modal-lg" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Seleccionar docente.</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <table id="tablaSeleccionDocentes" class="table table-striped table-bordered dt-responsive tablaletradocentes " cellspacing="0" width="100%">
-          <thead>
-            <tr>
-              <th>MATERIA</th>
-              <th>DEPARTAMENTO</th>
-              <th>OPCIONes</th>
-            </tr>
-          </thead>
-          <tbody>
-            <?php
-            foreach ($Docentes as $key => $valores) {
-              ?>
-              <tr>
-                <td>
-                  <?php echo "".utf8_decode($valores->nombres)." ".utf8_decode($valores->apellidos); ?>
-                </td>
-                <td>
-                  <?php echo "".utf8_decode($valores->departamento); ?>
-                </td>
-                <td>
-                  <center>
-                    <button type="button" onclick="selecionarDoc('<?php echo $valores->rfc; ?>','<?php echo "".utf8_decode($valores->nombres)." ".utf8_decode($valores->apellidos)?>')" class="btn btn-info" >
-                      <i class="fa  fa-check-square" aria-hidden="true"></i>
-
-                    </button>
-                  </center>
-                </td>
-              </tr>
-              <?php
-            }
-            ?>
-          </tbody>
-        </table>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-danger" data-dismiss="modal">CANCELAR</button>
+  <!-- MODAL DOCENTES FIN  -->
+  <!-- MODAL AGREGAR MATERIA  -->
+  <form method="post" id="formularioAgregarMateria">
+    <div class="modal fade" id="modalAgregarMateria" tabindex="-1" role="dialog" aria-labelledby="modalAgregarMateria" aria-hidden="true">
+      <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title"  >Agregar nueva materia</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <div class="row">
+              <div class="col-lg-3">
+                <label class="formulariolabel">Nombre de la materia:</label>
+              </div>
+              <div class="col-lg-9">
+                <input value="" name="nombre_materia" id="nombre_materia" placeholder="Nombre de la materia" class="form-control"  required/>
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-danger" data-dismiss="modal">CANCELAR</button>
+            <button  type="submit" class="btn btn-success">AGREGAR MATERIA</button>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
-<!-- MODAL DOCENTES FIN  -->
-<!-- MODAL AGREGAR MATERIA  -->
-<form method="post" id="formularioAgregarMateria">
-  <div class="modal fade" id="modalAgregarMateria" tabindex="-1" role="dialog" aria-labelledby="modalAgregarMateria" aria-hidden="true">
+  </form>
+  <!-- MODAL AGREGAR MATERIA  -->
+  <!-- MODAL SELECIONAR MATERIAS  -->
+  <div class="modal fade" id="modalSeleccionMateria" tabindex="-1" role="dialog" aria-labelledby="modalSeleccionMateria" aria-hidden="true">
     <div class="modal-dialog modal-lg" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title"  >Agregar nueva materia</h5>
+          <h5 class="modal-title">Seleccionar materia</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
         <div class="modal-body">
           <div class="row">
-            <div class="col-lg-3">
-              <label class="formulariolabel">Nombre de la materia:</label>
-            </div>
-            <div class="col-lg-9">
-              <input value="" name="nombre_materia" id="nombre_materia" placeholder="Nombre de la materia" class="form-control"  required/>
+            <div class="col-lg-12">
+              <table id="tablaSelecionarMaterias" class="table table-striped table-bordered dt-responsive " cellspacing="0" width="100%">
+                <thead>
+                  <tr>
+                    <th>MATERIA</th>
+                    <th>OPCION</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php
+                  foreach ($MateriasExistentes as $key => $valores) {
+                    ?>
+                    <tr>
+                      <td>
+                        <?php echo "".$valores->nombre_materia; ?>
+                      </td>
+                      <td>
+                        <center>
+                          <button type="button" onclick="selecionarMat(<?php echo $valores->idmaterias; ?>,'<?php echo $valores->nombre_materia; ?>')" class="btn btn-info " ><i class="fa  fa-check-square" aria-hidden="true"></i> SELECCIONAR</button>
+                        </center>
+                      </td>
+                    </tr>
+                    <?php
+                  }
+                  ?>
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-danger" data-dismiss="modal">CANCELAR</button>
-          <button  type="submit" class="btn btn-success">AGREGAR MATERIA</button>
         </div>
       </div>
     </div>
   </div>
-</form>
-<!-- MODAL AGREGAR MATERIA  -->
-<!-- MODAL SELECIONAR MATERIAS  -->
-<div class="modal fade" id="modalSeleccionMateria" tabindex="-1" role="dialog" aria-labelledby="modalSeleccionMateria" aria-hidden="true">
-  <div class="modal-dialog modal-lg" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Seleccionar materia</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <div class="row">
-          <div class="col-lg-12">
-            <table id="tablaSelecionarMaterias" class="table table-striped table-bordered dt-responsive " cellspacing="0" width="100%">
-              <thead>
-                <tr>
-                  <th>MATERIA</th>
-                  <th>OPCION</th>
-                </tr>
-              </thead>
-              <tbody>
-                <?php
-                foreach ($MateriasExistentes as $key => $valores) {
-                  ?>
-                  <tr>
-                    <td>
-                      <?php echo "".$valores->nombre_materia; ?>
-                    </td>
-                    <td>
-                      <center>
-                        <button type="button" onclick="selecionarMat(<?php echo $valores->idmaterias; ?>,'<?php echo $valores->nombre_materia; ?>')" class="btn btn-info " ><i class="fa  fa-check-square" aria-hidden="true"></i> SELECCIONAR</button>
-                      </center>
-                    </td>
-                  </tr>
-                  <?php
-                }
-                ?>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-danger" data-dismiss="modal">CANCELAR</button>
-      </div>
-    </div>
-  </div>
-</div>
-<!-- MODAL SELECIONAR MATERIAS  -->
-<?php $this->load->view('include/footer'); ?>
+  <!-- MODAL SELECIONAR MATERIAS  -->
+  <?php $this->load->view('include/footer'); ?>
 </body>
 <script type="text/javascript" src="<?php echo base_url(); ?>js/jquery-3.2.1.min.js"></script>
 <script type="text/javascript" src="<?php echo base_url(); ?>js/jquery.matchHeight.js"></script>
@@ -327,6 +328,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <script type="text/javascript" src="<?php echo base_url(); ?>js/responsive.bootstrap4.min.js"></script>
 <script>var urlsistema = '<?php echo base_url()?>';</script>
 <script type="text/javascript" src="<?php echo base_url(); ?>js/ssa.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>js/dataTables.checkboxes.min.js"></script>
 <script type="text/javascript" src="<?php echo base_url(); ?>js/ssatables.js"></script>
 <script>
 function selecionarMat(idmateria,nombre) {
--- js/ssatables.js
+++ js/ssatables.js
@@ -33,75 +33,111 @@ $(document).ready(function(){
     },
     "order": [[0, "asc" ]]
   });
+
   /*TABLA ALUMNOS*/
-  var table =  $('#tablaSeleccionAlumnos').DataTable({
+  var table = $('#tablaSeleccionAlumnos').DataTable({
     responsive: true,
     "language": {
       "url": urlsistema+"js/datatables/Alumnos.json"
     },
-    'columnDefs': [{
-      'targets': 0,
-      'searchable': false,
-      'orderable': false,
-      'className': 'dt-body-center',
-      'render': function (data, type, full, meta){
-        //return '<input type="checkbox" class="form-check-input position-static" name="id[]" value=" ' + $('<div/>').text(data).html() + '">';
-        return '<input type="checkbox" class=" position-static" name="id[]" value=" ' + $('<div/>').text(data).html() + '">';
-      }
-    }],
-    "order": [[2, "asc" ]]
-  });
-  $('#example-select-all').on('click', function(){
-    // Get all rows with search applied
-    var rows = table.rows({ 'search': 'applied' }).nodes();
-    // Check/uncheck checkboxes for all rows in the table
-    $('input[type="checkbox"]', rows).prop('checked', this.checked);
-  });
-  $('#example tbody').on('change', 'input[type="checkbox"]', function(){
-    // If checkbox is not checked
-    if(!this.checked){
-      var el = $('#example-select-all').get(0);
-      // If "Select all" control is checked and has 'indeterminate' property
-      if(el && el.checked && ('indeterminate' in el)){
-        // Set visual state of "Select all" control
-        // as 'indeterminate'
-        el.indeterminate = true;
-      }
-    }
-  });
-  // Handle form submission event
-  $('#formularioAlumnos').on('submit', function(e){
-    var form = this;
-    // Iterate over all checkboxes in the table
-    table.$('input[type="checkbox"]').each(function(){
-      // If checkbox doesn't exist in DOM
-      if(!$.contains(document, this)){
-        // If checkbox is checked
-        if(this.checked){
-          // Create a hidden element
-          $(form).append(
-            $('<input>')
-            .attr('type', 'hidden')
-            .attr('name', this.name)
-            .val(this.value)
-          );
+    "columnDefs": [
+      {
+        "targets": 0,
+        "checkboxes": {
+          "selectRow": true
         }
       }
-    });
-    function fixedEncodeURI (str) {
-      return encodeURI(str).replace(/%5B/g, '[').replace(/%5D/g, ']');
-    }
-    var values = {};
-    $.each($('#formularioAlumnos').serializeArray(), function(i, field) {
-      values[field.name] = field.value;
-    });
-    $('#example-console').text(values);
-    //$('#example-console').text($(form).serialize());
-    //  $('#example-console').text(fixedEncodeURI($(form).serialize()));
-    console.log("Form submission", $(form).serialize());
-    // Prevent actual form submission
-    e.preventDefault();
+    ],
+    "select": {
+      "style": "multi"
+    },
+    "order": [[2, "asc" ]]
   });
+
+  $('#formularioAlumnos').on('submit', function(e){
+    var form = this;
+    var rows_selected = table.column(0).checkboxes.selected();
+    $.each(rows_selected, function(index, rowId){
+      $(form).append(
+        $('<input>')
+        .attr('type', 'hidden')
+        .attr('name', 'id[]')
+        .val(rowId)
+      );
+    });
+    $('#example-console').text(rows_selected.join(","));
+     e.preventDefault();
+  });
+  //
+  // /*TABLA ALUMNOS*/
+  // var table =  $('#tablaSeleccionAlumnos').DataTable({
+  //   responsive: true,
+  //   "language": {
+  //     "url": urlsistema+"js/datatables/Alumnos.json"
+  //   },
+  //   'columnDefs': [{
+  //     'targets': 0,
+  //     'searchable': false,
+  //     'orderable': false,
+  //     'className': 'dt-body-center',
+  //     'render': function (data, type, full, meta){
+  //       //return '<input type="checkbox" class="form-check-input position-static" name="id[]" value=" ' + $('<div/>').text(data).html() + '">';
+  //       return '<input type="checkbox" class=" position-static" name="id[]" value=" ' + $('<div/>').text(data).html() + '">';
+  //     }
+  //   }],
+  //   "order": [[2, "asc" ]]
+  // });
+  // $('#example-select-all').on('click', function(){
+  //   // Get all rows with search applied
+  //   var rows = table.rows({ 'search': 'applied' }).nodes();
+  //   // Check/uncheck checkboxes for all rows in the table
+  //   $('input[type="checkbox"]', rows).prop('checked', this.checked);
+  // });
+  // $('#example tbody').on('change', 'input[type="checkbox"]', function(){
+  //   // If checkbox is not checked
+  //   if(!this.checked){
+  //     var el = $('#example-select-all').get(0);
+  //     // If "Select all" control is checked and has 'indeterminate' property
+  //     if(el && el.checked && ('indeterminate' in el)){
+  //       // Set visual state of "Select all" control
+  //       // as 'indeterminate'
+  //       el.indeterminate = true;
+  //     }
+  //   }
+  // });
+  // // Handle form submission event
+  // $('#formularioAlumnos').on('submit', function(e){
+  //   var form = this;
+  //   // Iterate over all checkboxes in the table
+  //   table.$('input[type="checkbox"]').each(function(){
+  //     // If checkbox doesn't exist in DOM
+  //     if(!$.contains(document, this)){
+  //       // If checkbox is checked
+  //       if(this.checked){
+  //         // Create a hidden element
+  //         $(form).append(
+  //           $('<input>')
+  //           .attr('type', 'hidden')
+  //           .attr('name', this.name)
+  //           .val(this.value)
+  //         );
+  //       }
+  //     }
+  //   });
+  //   function fixedEncodeURI (str) {
+  //     return encodeURI(str).replace(/%5B/g, '[').replace(/%5D/g, ']');
+  //   }
+  //   var values = {};
+  //   $.each($('#formularioAlumnos').serializeArray(), function(i, field) {
+  //     values[field.name] = field.value;
+  //   });
+  //   $('#example-console').text(values);
+  //   //$('#example-console').text($(form).serialize());
+  //   //  $('#example-console').text(fixedEncodeURI($(form).serialize()));
+  //   console.log("Form submission", $(form).serialize());
+  //   // Prevent actual form submission
+  //   e.preventDefault();
+  // });
   /*TABLA ALUMNOS*/
 });
 /*FIN JS*/
