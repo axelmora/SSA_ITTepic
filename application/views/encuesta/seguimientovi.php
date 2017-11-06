6690540074f29ecad0494c6c908d2828cfdbefa1
fernando <feranimaciones@gmail.com>
2017-11-05 22:11:18 -0700

    Act Scripts!

--- application/views/encuesta/seguimientovi.php
+++ application/views/encuesta/seguimientovi.php
@@ -594,5 +594,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 </div>
 </div>
 <script type="text/javascript" src="<?php echo base_url(); ?>js/jquery-3.2.1.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>js/popper.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>js/bootstrap.min.js"></script>
 </body>
 </html>
