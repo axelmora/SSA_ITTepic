7de4c06b70e41fd861a26514dfe8196811b834df
fernando mac <feranimaciones@gmail.com>
2017-12-11 18:48:46 -0700

    act

--- application/views/generar_imagen.php
+++ application/views/generar_imagen.php
@@ -27,7 +27,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	}else {
 		if($this->session->userdata('tipo')=='3')
 		{
-			 $this->load->view('include/menuadministrativo');
+			$this->load->view('include/menuadministrativo');
 		}else {
 			$this->load->view('include/menuadmin');
 		}
@@ -38,53 +38,57 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			<div class="col-lg-12">
 				<div class="card sombrapaneles">
 					<div class="card-body cuerpo"   >
-						<h3> <a class="btn btn-naranja" data-toggle="tooltip" data-placement="top" title="Volver" href="<?php echo base_url(); ?>index.php/Panel_seguimiento/aplicaciones" role="button"><i class="fa fa-undo" aria-hidden="true"></i></a> <i class="fa fa-picture-o" aria-hidden="true"></i> Cartel promocion</h3>
-						<center>
-							<img class="img-fluid" src="<?php echo base_url(); ?><?php echo $cartel;?>" width="1920" height="1080"/>
-							<a  download="cartel_promocional" class="btn btn-naranja hvr-pulse-grow" data-toggle="tooltip" data-placement="top" title="Descargar cartel promocional" href="<?php echo base_url(); ?><?php echo $cartel;?>" role="button"><i class="fa fa-download" aria-hidden="true"></i> Descargar cartel promocional</a>
-						</center>
+						<h3>
+							<a class="btn btn-naranja" data-toggle="tooltip" data-placement="top" title="Volver" href="<?php echo base_url(); ?>index.php/Panel_seguimiento/aplicaciones" role="button">
+								<i class="fa fa-undo" aria-hidden="true"></i>
+							</a>
+							<i class="fa fa-picture-o" aria-hidden="true"></i> Cartel promocion</h3>
+							<center>
+								<img class="img-fluid" src="<?php echo base_url(); ?><?php echo $cartel;?>" width="1920" height="1080"/>
+								<a  download="cartel_promocional" class="btn btn-naranja hvr-pulse-grow" data-toggle="tooltip" data-placement="top" title="Descargar cartel promocional" href="<?php echo base_url(); ?><?php echo $cartel;?>" role="button"><i class="fa fa-download" aria-hidden="true"></i> Descargar cartel promocional</a>
+							</center>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-	<?php $this->load->view('include/manual_usuario'); ?>
-	<?php $this->load->view('include/footer'); ?>
-</body>
-<script type="text/javascript" src="<?php echo base_url(); ?>js/jquery-3.2.1.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>js/jquery.matchHeight.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>js/tether.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>js/popper.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>js/bootstrap.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>js/jquery.validate.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>js/ssa-validador.js"></script>
-<script type="text/javascript">var urlsistema = '<?php echo base_url()?>';</script>
-<script type="text/javascript" src="<?php echo base_url(); ?>js/ssa.js"></script>
-<script>
-$(document).ready(function(){
-	var opciones = {
-		fallbackLink: '<p>El navegador no soporta este manual  <center><a href="[url]"  class="btn btn-primary" download><i class="fa fa-download" aria-hidden="true"></i> DESCARGAR MANUAL</a></center></p>'
-	};
-	PDFObject.embed("<?php echo base_url(); ?>file/manual/Manual_Usuario_SSA.pdf","#manualdeusuariover", opciones);
-	$(function () {
-		$('[data-toggle="tooltip"]').tooltip()
-	})
-	var estado=true;
-	$("#botonEditar").click(function(){
-		if(estado)
-		{
-			$("#nombre_usuario").prop('disabled', false);
-			$( "#botonSubmit" ).slideDown( "fast", function() {
-				estado=false;
-			});
-		}else {
-			$("#nombre_usuario").prop('disabled', true);
-			$( "#botonSubmit" ).slideUp( "fast", function() {
-				estado=true;
-			});
-		}
+		<?php $this->load->view('include/manual_usuario'); ?>
+		<?php $this->load->view('include/footer'); ?>
+	</body>
+	<script type="text/javascript" src="<?php echo base_url(); ?>js/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="<?php echo base_url(); ?>js/jquery.matchHeight.js"></script>
+	<script type="text/javascript" src="<?php echo base_url(); ?>js/tether.min.js"></script>
+	<script type="text/javascript" src="<?php echo base_url(); ?>js/popper.min.js"></script>
+	<script type="text/javascript" src="<?php echo base_url(); ?>js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="<?php echo base_url(); ?>js/jquery.validate.js"></script>
+	<script type="text/javascript" src="<?php echo base_url(); ?>js/ssa-validador.js"></script>
+	<script type="text/javascript">var urlsistema = '<?php echo base_url()?>';</script>
+	<script type="text/javascript" src="<?php echo base_url(); ?>js/ssa.js"></script>
+	<script>
+	$(document).ready(function(){
+		var opciones = {
+			fallbackLink: '<p>El navegador no soporta este manual  <center><a href="[url]"  class="btn btn-primary" download><i class="fa fa-download" aria-hidden="true"></i> DESCARGAR MANUAL</a></center></p>'
+		};
+		PDFObject.embed("<?php echo base_url(); ?>file/manual/Manual_Usuario_SSA.pdf","#manualdeusuariover", opciones);
+		$(function () {
+			$('[data-toggle="tooltip"]').tooltip()
+		})
+		var estado=true;
+		$("#botonEditar").click(function(){
+			if(estado)
+			{
+				$("#nombre_usuario").prop('disabled', false);
+				$( "#botonSubmit" ).slideDown( "fast", function() {
+					estado=false;
+				});
+			}else {
+				$("#nombre_usuario").prop('disabled', true);
+				$( "#botonSubmit" ).slideUp( "fast", function() {
+					estado=true;
+				});
+			}
+		});
 	});
-});
 </script>
 <?php
 if(isset($errorSubmit))
