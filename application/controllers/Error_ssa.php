b6b494d83e7d7bd1578050b5e721e97d3c0b20e8
fernando <feranimaciones@gmail.com>
2017-12-19 22:04:00 -0700

    act

--- application/controllers/Error_ssa.php
+++ application/controllers/Error_ssa.php
@@ -9,7 +9,7 @@ class Error_ssa extends CI_Controller {
   public function index()
   {
     $this->output->set_status_header('404');
-    $data['content'] = 'error_404'; // View name
+    $data['content'] = 'error_404';
     $this->load->view('error_404_ssa',$data);
   }
 }
