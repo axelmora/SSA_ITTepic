438fb27dbfb1e4361384c62d79367dae4069f29f
fernando <feranimaciones@gmail.com>
2018-01-21 22:12:21 -0700

    act

--- application/views/administracion/vpanel_administracion_departamenos_nuevo.php
+++ application/views/administracion/vpanel_administracion_departamenos_nuevo.php
@@ -54,7 +54,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 												{
 													foreach ($CARRERAS as $key => $value) {
 														 ?>
-														 	<option value="<?php echo $value->id_carrera; ?>" ><?php echo $value->id_carrera."-".$value->carrera ; ?></option>
+														 	<option value="<?php echo $value->id_carrera; ?>" ><?php echo $value->id_carrera."-".mb_convert_encoding($value->carrera, 'Windows-1252') ; ?></option>
 														 <?php
 													}
 												}
